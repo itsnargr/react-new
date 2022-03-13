b568f73fb5c3d7b73cb39c02da98eb25112e2fb2
narges <itsnrgr@gmail.com>
2022-03-13 09:21:57 +0330

    data

--- src/Form.js
+++ src/Form.js
@@ -1,29 +1,8 @@
 import React from "react";
-import "./styles.css";
-export default function Form() {
-  return (
-    <div>
-      <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-        rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-        crossorigin="anonymous"
-      ></link>
-      <div className="container">
-        <div className="weather-app">
-          <form id="search-form">
-            <input
-              type="search"
-              placeholder="City"
-              autofocus="on"
-              className="search-bar"
-              id="input-city"
-              autocomplete="off"
-            />
-            <input type="submit" value="Search" class="btn btn-secondary" />
-          </form>
-        </div>
-      </div>
-    </div>
-  );
-}
+import ReactDOM from "react-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/Weather.js
+++ src/Weather.js
@@ -1,49 +1,105 @@
-import React from "react";
+
 import Form from "./Form";
+import React, { useState } from "react";
+
+import axios from "axios";
 import "./styles.css";
 
-export default function Weather() {
-  let weatherData = {
-    city: "tehran",
-    temperature: "33 ",
-    status: "windy",
-    time: "tuesday",
-    humidity: "70",
-    wind: "55 km/h",
-  };
-  return (
-    <div className="body">
-      <Form />
-      <div className="overview">
-        <h1>Tehran</h1>
-        <ul className="nonstyle">
-          <li> city :{weatherData.city}</li>
-          <li> status {weatherData.status}</li>
-        </ul>
-      </div>
-      <div className="row">
-        <div className="col-6">
-          <div className="clearfix weather-temperature">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-              alt="sunny icon"
-              className="float-left"
-            />
-            <div className="float-left">
-              <strong> temperature : {weatherData.temperature}</strong>
-              <span className="units">
-                <a href="/">°C</a> | <a href="/">°F</a>
-              </span>
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
+
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      coordinates: response.data.coord,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  function search() {
+    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="body">
+        <div className="Weather">
+          <form onSubmit={handleSubmit}>
+            <div className="row">
+              <div className="col-9">
+                <input
+                  type="search"
+                  placeholder="Enter a city.."
+                  className="form-control"
+                  autoFocus="on"
+                  onChange={handleCityChange}
+                />
+              </div>
+              <div className="col-3">
+                <input
+                  type="submit"
+                  value="Search"
+                  className="btn btn-primary w-100"
+                />
+              </div>
             </div>
-          </div>
+          </form>
+          <WeatherInfo data={weatherData} />
+          <WeatherForecast coordinates={weatherData.coordinates} />
         </div>
-        <div className="col-6">
+        );
+        <div className="overview">
+          <h1>Tehran</h1>
           <ul className="nonstyle">
-            <li> humidity :{weatherData.humidity}</li>
-            <li> wind :{weatherData.wind}</li>
+            <li> city :{city}</li>
+            <li> status {weatherData.description}</li>
           </ul>
         </div>
+        <div className="row">
+          <div className="col-6">
+            <div className="clearfix weather-temperature">
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+                alt="sunny icon"
+                className="float-left"
+              />
+              <div className="float-left">
+                <strong> temperature : {weatherData.temperature}</strong>
+                <span className="units">
+                  <a href="/">°C</a> | <a href="/">°F</a>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <ul className="nonstyle">
+              <li> humidity :{weatherData.humidity}</li>
+              <li> wind :{weatherData.wind}</li>
+            </ul>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
 }
